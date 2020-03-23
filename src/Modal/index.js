30f6b8e2e9bab013b55cc0237196e4e51974b911
Auspicus <elijahdisch@gmail.com>
2020-03-23 10:28:19 +1000

    Add modal

---
+++ src/Modal/index.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types'
+import './styles.css'
+
+const Modal = ({
+  title,
+  subtitle,
+}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Modal.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+}
+
+export default Modal
